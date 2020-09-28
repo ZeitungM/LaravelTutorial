c14b2c619ac9da8841d77c0fac12a85620d9d623
ZeitungM <http418im.a.teapot@gmail.com>
2020-09-28 15:09:39 +0000

    フォルダ内のタスクを表示する機能を作った

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -11,8 +12,12 @@ class TaskController extends Controller
     public function index(int $id)
     {
         $folders = Folder::all();
+        $current_folder = Folder::find($id);
 
-        return view('tasks/index', [ 'folders' => $folders,
-                                     'current_folder_id' => $id, ] );
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+        return view('tasks/index', [ 'folders'           => $folders,
+                                     'current_folder_id' => $id,
+                                     'tasks'             => $tasks, ] );
     }
 }
