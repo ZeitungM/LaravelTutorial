f973a1ea6f4e265865d30173b18425c75f973b85
ZeitungM <http418im.a.teapot@gmail.com>
2020-10-15 15:08:33 +0000

    タスク追加処理を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Http\Requests\CreateTask;
 use App\Task;
 use Illuminate\Http\Request;
 
@@ -24,5 +25,18 @@ class TaskController extends Controller
     public function showCreateForm(int $folder_id)
     {
         return view( 'tasks/create', [ 'folder_id' => $folder_id ] );
+    }
+
+    public function create(int $folder_id, CreateTask $request)
+    {
+        $current_folder = Folder::find($folder_id);
+        
+        $task           = new Task();
+        $task->title    = $request->title;
+        $task->due_date = $request->due_date;
+        
+        $current_folder->tasks()->save($task);
+        
+        return redirect()->route( 'tasks.index', ['id'=>$current_folder->id, ] );
     }
 }
