198a42352bf2057ab0fabf0df86102f87f742c10
ZeitungM <http418im.a.teapot@gmail.com>
2020-11-20 17:56:32 +0000

    flatpicker のデグレを修正

--- resources/views/share/flatpickr/styles.blade.php
+++ resources/views/share/flatpickr/styles.blade.php
@@ -1,2 +1,2 @@
-<link rel="sytlesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-<link rel="sytlesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
